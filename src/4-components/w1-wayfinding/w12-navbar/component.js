14443d6795ee3bb393515cff3906a5bd34546c5a
jyesberg <joshua.yesberg@act.gov.au>
2023-06-09 14:18:34 +1000

    revert removal of the .act-h1 etc classes remove act-h5 class from the nav icons, applied the styles directly to them to get around the margin-bottom:0 :last-child issue

--- src/4-components/w1-wayfinding/w12-navbar/component.js
+++ src/4-components/w1-wayfinding/w12-navbar/component.js
@@ -21,13 +21,13 @@ export const createW12 = ({
       </div>
       <div class="act-navbar__content">
         <div class="act-navbar__search">
-          <div class="act-navbar__search__container act-h5">
+          <div class="act-navbar__search__container">
             <input class="act-navbar__search__text" placeholder="Search"/>
             <i class="fa-solid fa-magnifying-glass"></i>
           </div>
         </div>
         <div class="act-navbar__menu">
-          <div class="act-navbar__menu__container act-h5">
+          <div class="act-navbar__menu__container">
             <span class="act-navbar__menu__text">Menu</span> <i class="fa-solid fa-bars"></i>
           </div>
         </div>
