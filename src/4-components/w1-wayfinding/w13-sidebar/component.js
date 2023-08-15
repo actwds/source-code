e491ce267d243ff4d1db80293773b1829781f25f
jyesberg <joshua.yesberg@act.gov.au>
2023-08-15 15:50:06 +1000

    Various updates to sidebar, pagination, and container padding

--- src/4-components/w1-wayfinding/w13-sidebar/component.js
+++ src/4-components/w1-wayfinding/w13-sidebar/component.js
@@ -7,7 +7,7 @@ export const createW13 = () => {
   </div>
   <details class="act-sidebar__item">
     <summary class="act-sidebar__item__summary">
-      Dropdown
+      <a href="#">Dropdown</a>
       <div class="summary-chevron">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
       </div>
@@ -27,7 +27,7 @@ export const createW13 = () => {
   
   <details class="act-sidebar__item">
     <summary class="act-sidebar__item__summary">
-      Dropdown
+      <a href="#">Dropdown</a>
       <div class="summary-chevron">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
       </div>
--- src/_scripts/spf3.js
+++ src/_scripts/spf3.js
@@ -308,12 +308,12 @@ document.addEventListener("DOMContentLoaded", function() {
 	const megaMenuLeft = document.getElementsByClassName("act-megamenu__content__block-main-menu")[0];
 	megaMenuLeft.addEventListener("click", function(e) {
 		if (e.target.closest(".act-megamenu__link")) {
-			//do nothing
-		} else {
-			document.getElementsByClassName("act-megamenu__content__block-sub-menu")[0].classList.add("hidden-mobile");
-			Array.from(document.getElementsByClassName("act-megamenu__sub-menu-link-container")).forEach(function(container) {
-				container.style.display = "none";
-			});
-		}
+			return;
+		} 
+		document.getElementsByClassName("act-megamenu__content__block-sub-menu")[0].classList.add("hidden-mobile");
+		Array.from(document.getElementsByClassName("act-megamenu__sub-menu-link-container")).forEach(function(container) {
+			container.style.display = "none";
+		});
+		
 	});
 });
