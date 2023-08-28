e89c210fe4d9c90238d0e4718971a0d4818cc021
jyesberg <joshua.yesberg@act.gov.au>
2023-08-29 08:31:42 +1000

    Various updates to hero search row

--- src/4-components/h1-hero-rows/h14-home-search-row/component.js
+++ src/4-components/h1-hero-rows/h14-home-search-row/component.js
@@ -5,22 +5,28 @@ export const createH14 = () => {
   const searchBarHTML = createS12();
 
   return `
-<div class="act-hero-row__search">
-  <div class="act-hero-row__search__container">
-    <div class="act-col__cw-12">
-      <div class="act-box">
-        <div class="act-hero-row__search__position">
-          <div class="act-hero-row__search__frame">
-            <div class="act-col__cw-6">
-              <h3>An easier way to access government services</h3>
-            </div>
-            <div class="act-col__cw-6">
-              `+searchBarHTML+`
-            </div>
+<div class="act-box">
+  <div class="act-hero-row__search">
+    <div class="act-hero-row__search__container">
+      <div class="act-col__cw-12">
+        <div class="act-box">
+          <div class="act-hero-row__search__position">           
           </div>
         </div>
       </div>
     </div>
   </div>
+  <div class="act-hero-row__container">
+    <div class="act-hero-row__search__frame">
+      <div class="act-col__cw-6">
+        <h3>An easier way to access government services</h3>
+      </div>
+      <div class="act-col__cw-6">
+        `+searchBarHTML+`
+      </div>
+    </div>
+  </div>         
 </div>
 `;};
+
+
