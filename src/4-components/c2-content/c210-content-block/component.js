61f193c21915cd044d05142381e245734c280088
jyesberg <joshua.yesberg@act.gov.au>
2024-02-15 08:52:03 +1100

    Added a content block component and more spacing variables

---
+++ src/4-components/c2-content/c210-content-block/component.js
@@ -0,0 +1,91 @@
+import { createT31 } from "../../t3-tags/t31-tags/component";
+
+export const createC210 = ({
+  heading,
+  iconVisible,
+  icon,
+  imageVisible,
+  description,
+  linksVisible,
+  links1,
+  links2,
+  links3,
+  links4,
+}) => {
+  let iconVisibleHTML = ``;
+  let featureIconHTML = ``;
+  let imageVisibleHTML = ``;
+  let featureImageHTML = ``;
+  let linksListVisibleHTML = ``;
+  let featureDescriptionHTML = description;
+  let linksHTML = ``;
+
+  if (iconVisible == true) {
+    iconVisibleHTML = `act-content-block__feature--icon`;
+    featureIconHTML = `<div class="act-content-block__icon"><z class="fa-solid `+icon+`"></z></div>`;
+  } else {
+    iconVisibleHTML = ``;
+    featureIconHTML = ``;
+  }
+
+  if (imageVisible == true) {
+    imageVisibleHTML = `act-content-block__feature--image`;
+    featureImageHTML = `<div class="act-content-block__image" style="background-image: url('/images/purple.jpg');"></div>`;
+  } else {
+    imageVisibleHTML = ``;
+    featureImageHTML = ``;
+  }
+
+  if (linksVisible == true) {
+    linksListVisibleHTML = `act-content-block__feature--links-list`;
+    linksHTML = `
+    <div class="act-links-list">
+      <div class="act-links-list__container">
+        <ul class="act-links-list__content">
+          <li class="act-links-list__content__item">
+            <a href="#" class="act-links-list__content__item__text">`+links1+`
+              <div class="act-links-list__content__item__icon">
+                <z class="fa-solid fa-arrow-right"></z>
+              </div>
+            </a>
+          </li>
+          <li class="act-links-list__content__item">
+            <a href="#" class="act-links-list__content__item__text">`+links2+`
+              <div class="act-links-list__content__item__icon">
+                <z class="fa-solid fa-arrow-right"></z>
+              </div>
+            </a>
+          </li>
+          <li class="act-links-list__content__item">
+            <a href="#" class="act-links-list__content__item__text">`+links3+`
+              <div class="act-links-list__content__item__icon">
+                <z class="fa-solid fa-arrow-right"></z>
+              </div>
+            </a>
+          </li>
+          <li class="act-links-list__content__item">
+            <a href="#" class="act-links-list__content__item__text">`+links4+`
+              <div class="act-links-list__content__item__icon">
+                <z class="fa-solid fa-arrow-right"></z>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>`;
+  }
+
+  return `
+  <div class="act-content-block `+imageVisibleHTML+` `+iconVisibleHTML+` `+linksListVisibleHTML+`">
+    <div class="act-content-block__container">
+      <div class="act-content-block__content">
+        `+featureIconHTML+`
+        `+featureImageHTML+`
+        <span class="act-content-block__title">`+heading+`</span>
+        <span class="act-content-block__description">`+featureDescriptionHTML+`</span>
+      </div>
+      `+linksHTML+`
+    </div>
+  </div>
+`;
+};
--- src/6-previews/pr2-access-canberra/component.js
+++ src/6-previews/pr2-access-canberra/component.js
@@ -4,6 +4,7 @@ import { createPC11 } from "../../5-patterns/pc1-card-grid/pc11-default-card-gri
 import { createPT1 } from "../../5-patterns/pt1-topbar/component";
 import { createPF2 } from "../../5-patterns/pf2-footers/component";
 import { createC31 } from "../../4-components/c3-case-study/c31-case-study/component";
+import { createC210 } from "../../4-components/c2-content/c210-content-block/component";
 
 
 export const createPR2 = () => {
@@ -53,6 +54,19 @@ export const createPR2 = () => {
     buttonVisible: true,
   });
 
+  const contentBlock = createC210({
+    heading: "Content block",
+    iconVisible: true,
+    icon: "fa-pizza-slice",
+    imageVisible: true,
+    linksVisible: true,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a ipsum a felis porta vulputate. Etiam condimentum blandit tellus, in molestie purus fermentum et. Nunc vestibulum sapien sit amet ex facilisis, et malesuada sapien malesuada.",
+    links1: "Link #1",
+    links2: "Link #2",
+    links3: "Link #3",
+    links4: "Link #4",
+  });
+
   const topbarHTML = createPT1({
     title: "An ACT Government Website",
     buttonText: "Log In",
@@ -79,7 +93,11 @@ export const createPR2 = () => {
           `
           <h2>Case study</h2>`
           + caseStudy +
-          `</div>
+          `
+          <h2>Content block</h2>`
+          + contentBlock +
+          `
+          </div>
           </div>
         </div>
       </div>
