de53737c88843e6e5cd0a4dded5fa63df6affe57
jyesberg <joshua.yesberg@act.gov.au>
2024-02-27 10:44:27 +1100

    Removed left indent from links list, updated grid links list to stack at mobile resolution

--- src/4-components/c2-content/c29-links-list/component.js
+++ src/4-components/c2-content/c29-links-list/component.js
@@ -9,38 +9,36 @@ export const createC29= ({
 
   return `
 <div class="act-links-list">
-  <div class="act-links-list__container">
-    <ul class="act-links-list__content`+typeHTML+`">
-      <li class="act-links-list__content`+typeHTML+`__item">
-        <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #1
-          <div class="act-links-list__content`+typeHTML+`__item__icon">
-            <z class="fa-solid fa-arrow-right"></z>
-          </div>
-        </a>
-      </li>
-      <li class="act-links-list__content`+typeHTML+`__item">
-        <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #2
-          <div class="act-links-list__content`+typeHTML+`__item__icon">
-            <z class="fa-solid fa-arrow-right"></z>
-          </div>
-        </a>
-      </li>
-      <li class="act-links-list__content`+typeHTML+`__item">
-        <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #3
-          <div class="act-links-list__content`+typeHTML+`__item__icon">
-            <z class="fa-solid fa-arrow-right"></z>
-          </div>
-        </a>
-      </li>
-      <li class="act-links-list__content`+typeHTML+`__item">
-        <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #4
-          <div class="act-links-list__content`+typeHTML+`__item__icon">
-            <z class="fa-solid fa-arrow-right"></z>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
+  <ul class="act-links-list__content`+typeHTML+`">
+    <li class="act-links-list__content`+typeHTML+`__item">
+      <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #1
+        <div class="act-links-list__content`+typeHTML+`__item__icon">
+          <z class="fa-solid fa-arrow-right"></z>
+        </div>
+      </a>
+    </li>
+    <li class="act-links-list__content`+typeHTML+`__item">
+      <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #2
+        <div class="act-links-list__content`+typeHTML+`__item__icon">
+          <z class="fa-solid fa-arrow-right"></z>
+        </div>
+      </a>
+    </li>
+    <li class="act-links-list__content`+typeHTML+`__item">
+      <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #3
+        <div class="act-links-list__content`+typeHTML+`__item__icon">
+          <z class="fa-solid fa-arrow-right"></z>
+        </div>
+      </a>
+    </li>
+    <li class="act-links-list__content`+typeHTML+`__item">
+      <a href="#" class="act-links-list__content`+typeHTML+`__item__text">Link #4
+        <div class="act-links-list__content`+typeHTML+`__item__icon">
+          <z class="fa-solid fa-arrow-right"></z>
+        </div>
+      </a>
+    </li>
+  </ul>
 </div>
 `;
 };
