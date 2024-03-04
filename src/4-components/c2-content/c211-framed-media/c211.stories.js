05c58187273c1f43e9e71cd5768e79fdc92a556d
jyesberg <joshua.yesberg@act.gov.au>
2024-03-04 14:22:31 +1100

    Added framed media component

---
+++ src/4-components/c2-content/c211-framed-media/c211.stories.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { createC211 } from "./component";
+import directory from "../../../_docs/directory.json";
+
+export default {
+	title: "Components/Content/Framed Media",
+	parameters: {
+		docs: {
+			description: {
+				component: "",
+			},
+		},
+		layout: "fullscreen",
+	},
+	argTypes: {
+		type: {
+			control: { type: "radio", },
+			options: ["YouTube", "Image", "Vimeo",],
+			defaultValue: "YouTube",
+			description: "Sets the media type",
+			table: {
+				defaultValue: {
+					summary: "YouTube",
+				},
+			},
+		},
+		youtube: {
+			control: { type: "text", },
+			defaultValue: "https://www.youtube.com/embed/K9aEw0G5fzs?si=uL0_3K6bM50VutIr",
+			table: {
+				defaultValue: {
+					summary: "https://www.youtube.com/embed/K9aEw0G5fzs?si=uL0_3K6bM50VutIr",
+				},
+			},
+		},
+		image: {
+			control: { type: "text", },
+			defaultValue: "/images/purple.jpg",
+			table: {
+				defaultValue: {
+					summary: "/images/purple.jpg",
+				},
+			},
+		},
+		vimeo: {
+			control: { type: "text", },
+			defaultValue: "https://player.vimeo.com/video/64699699?h=966c67b1bd",
+			table: {
+				defaultValue: {
+					summary: "https://player.vimeo.com/video/64699699?h=966c67b1bd",
+				},
+			},
+		},
+		caption: {
+			control: { type: "text", },
+			defaultValue: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			description: "Sets the text for the description",
+			table: {
+				defaultValue: {
+					summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+				},
+			},
+		},
+	},
+};
+
+const Template = (args) => createC211(args);
+
+export const FramedMedia = Template.bind({});
---
+++ src/4-components/c2-content/c211-framed-media/component.js
@@ -0,0 +1,43 @@
+export const createC211 = ({
+  caption,
+  type,
+  youtube,
+  image,
+  vimeo,
+}) => {
+  let youtubeHTML = ``;
+  let imageHTML = ``;
+  let vimeoHTML = ``;
+  let captionHTML = caption;
+
+  if (type == 'YouTube') {
+    youtubeHTML = `<iframe src="${youtube}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+  } else {
+    youtubeHTML = ``;
+  }
+
+  if (type == 'Image') {
+    imageHTML = `<img src='/images/purple.jpg'>`;
+  } else {
+    imageHTML = ``;
+  }
+
+  if (type == 'Vimeo') {
+    vimeoHTML = `<iframe src="${vimeo}" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen=""></iframe>`;
+  } else {
+    vimeoHTML = ``;
+  }
+
+  return `
+  <div class="act-framed-media">
+  <div class="act-framed-media__container">  
+      ${youtubeHTML}
+      ${imageHTML}
+      ${vimeoHTML}
+  </div>
+  <div class="act-framed-media__caption">${captionHTML}</div>
+</div>
+`;
+};
+
+
