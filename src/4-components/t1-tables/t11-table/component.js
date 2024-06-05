39b0f61e80b3aa6cf52b2c0d8c88205ecca77ba1
Joshua <joshua.yesberg@outlook.com>
2024-06-05 14:42:15 +1000

    Added a toggle for adding an overflow scroll div to the table component (#23)
    * Added a toggle for adding an overflow scroll div to the table component
    
    * Removed div::-webkit-scrollbar style from multistep component
    
    * Removed feature keyword from inside table component HTML
    
    * Reset word wrap/break styles on table component
    
    * Added placeholder scrollbar styles to tables component
    
    * Added ability to change content block heading level (#25)
    
    * Added Timeline component (#26)
    
    * Added a timeline component
    
    * Added a reference to the new timeline component scss
    
    * Added a directory entry for the new timeline component
    
    * Removed placeholder guidance page
    
    * Added timeline component to components overview page
    
    * Added a toggle for adding an overflow scroll div to the table component
    
    * Removed div::-webkit-scrollbar style from multistep component
    
    * Removed feature keyword from inside table component HTML
    
    * Reset word wrap/break styles on table component
    
    * Added placeholder scrollbar styles to tables component
    
    * Removed minified files
    
    ---------
    
    Co-authored-by: pbridgfoot <167831515+pbridgfoot@users.noreply.github.com>

--- src/4-components/t1-tables/t11-table/component.js
+++ src/4-components/t1-tables/t11-table/component.js
@@ -1,13 +1,23 @@
 export const createT11 = ({
   type,
+  feature,
 }) => {
 
+  var overflowClass = "";
+
+  if (feature == "overflow-div") {
+    overflowClass = "act-table-container__overflow";
+  }
+
   return `
-<div class="act-table-container">
+<div class="act-table-container `+overflowClass+`">
   <table class="act-table__`+type+`">
     <thead>
       <tr>
         <th>Learner licence fees</th>
+        <th>Fee</th>
+        <th>Fee</th>
+        <th>Fee</th>
         <th>Fee</th>
         <th>Fee</th>
       </tr>
@@ -17,14 +27,23 @@ export const createT11 = ({
         <td>Issue of a learner driver licence</td>
         <td>$51.10</td>
         <td>$51.10</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
       </tr>
       <tr>
         <td>Renewal of learner driver licence</td>
         <td>$51.10</td>
         <td>$51.10</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
       </tr>
       <tr>
         <td>Issue of a learner rider licence</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
         <td>$25.50</td>
         <td>$25.50</td>
       </tr>
@@ -32,11 +51,17 @@ export const createT11 = ({
         <td>Renewal of a learner rider licence</td>
         <td>$25.50</td>
         <td>$25.50</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
+        <td>$25.50</td>
       </tr>
     </tbody>
     <tfoot>
       <tr>
         <th>Learner licence fees</th>
+        <th>Fee</th>
+        <th>Fee</th>
+        <th>Fee</th>
         <th>Fee</th>
         <th>Fee</th>
       </tr>
