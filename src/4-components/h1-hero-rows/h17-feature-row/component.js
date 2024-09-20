19939fabbbb27994a96d638c4576a258bf40b6a6
pbridgfoot <bridgfootp@gmail.com>
2024-09-20 15:12:04 +1000

    Added feature row

---
+++ src/4-components/h1-hero-rows/h17-feature-row/component.js
@@ -0,0 +1,35 @@
+export const createH17 = () => {
+
+  return `
+  <div class="act-flex">
+                <div class="act-flex__container padding-none">
+                    <div class="act-flex__grid">
+                        <div class="act-flex__row">
+                            <div class="act-col__cw-12">
+                              <div class="act-box act-card__featured-home" style="background-image: url('/images/purple.jpg');">
+                              <div class="act-card-grid">
+                                  <div class="act-card act-card__featured" title="Card Link">
+                                    <div class="act-card__container">
+                                      <div class="act-card__content">
+                                        <div class="act-card__title">
+                                          <h2>Heading</h2>
+                                        </div>
+                                        <div class="act-card__description">
+                                          Description
+                                        </div>
+                                        <div class="act-card__cta">
+                                            <a href="#" class="act-button act-button__secondary act-button__default act-button__no-icon">
+                                              button text
+                                            </a>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+`;};
